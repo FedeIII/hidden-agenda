1a290cf0ae0977ea4f1c54c9b1b1fb246c45449f
Federico Perez <federico.perez@rumbo.com>
2017-01-02 21:34:53 +0100

    removed commented code

--- src/client/components/hq.jsx
+++ src/client/components/hq.jsx
@@ -32,35 +32,4 @@ function Hq ({
 //     pieces: PropTypes.object
 // };
 
-// class Hq extends React.Component {
-//     renderPieces() {
-//         const pieces = [];
-//         for (let pieceName in this.props.pieces) {
-//             const piece = this.props.pieces[pieceName];
-//             const PieceComponent = <Piece
-//                                         direction={piece.direction}
-//                                         // selected={this.props.isPieceSelected}
-//                                         selectPiece={() => this.props.selectPiece(piece)}
-//                                         deselectPiece={() => this.props.deselectPiece()}
-//                                     />
-//             pieces.push(PieceComponent);
-//         }
-//
-//         return pieces;
-//     }
-//
-//     render() {
-//         const className = `hq hq-${this.props.team}`;
-//         const pieces = this.renderPieces();
-//
-//         return (
-//             <div key={`team${this.props.team}`} className={className}>
-//                 <div className="hq__store">
-//                     {pieces}
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
 export default Hq;
