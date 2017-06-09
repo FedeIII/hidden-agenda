702ab7445efa1b0f5b709de7d9f7c7cff62bace8
Federico Perez <federico.perez@rumbo.com>
2017-06-09 21:01:17 +0200

    fixed follow mouse state calculation

--- src/client/reducers/followMouseReducer.js
+++ src/client/reducers/followMouseReducer.js
@@ -1,8 +1,10 @@
-import {TOGGLE_PIECE, MOVE_PIECE} from 'client/actions';
+import {MOVE_PIECE, DIRECT_PIECE} from 'client/actions';
 
 export default function followMouseReducer (state, action) {
     switch (action.type) {
         case MOVE_PIECE:
+            return true;
+        case DIRECT_PIECE:
             return true;
         default:
             return false;
