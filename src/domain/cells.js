c8342ad0bb354c1178dd80827ed9e410122e5d93
Federico Perez <fedelll@gmail.com>
2019-08-05 22:30:47 +0200

    formatted file

--- src/domain/cells.js
+++ src/domain/cells.js
@@ -1,4 +1,4 @@
-import {areCoordsEqual} from 'Domain/utils';
+import { areCoordsEqual } from 'Domain/utils';
 
 //           0,0  0,1  0,2  0,3
 //        1,0  1,1  1,2  1,3  1,4
@@ -12,114 +12,125 @@ const cellsByRow = [4, 5, 6, 7, 6, 5, 4];
 const cells = [];
 const outPosition = [-1, -1];
 
-function createGetPositionInDirection (r, c) {
-    return function getPositionInDirection ([v, h] = []) {
-        let hDiff = (h === 0) ? 1 : -1;
-        if (r < 3) {
-            if (v > 0) {
-                hDiff = -h;
-            } else if (v < 0) {
-                hDiff = +!h;
-            }
-        } else if (r === 3) {
-            if (v !== 0) {
-                hDiff = -h;
-            }
-        } else if (r > 3) {
-            if (v > 0) {
-                hDiff = +!h;
-            } else if (v < 0) {
-                hDiff = -h;
-            }
-        }
-
-        const coords = [r - v, c + hDiff];
-
-        if (inBoard(coords)) {
-            return coords;
-        }
+function createGetPositionInDirection(r, c) {
+  return function getPositionInDirection([v, h] = []) {
+    let hDiff = h === 0 ? 1 : -1;
+    if (r < 3) {
+      if (v > 0) {
+        hDiff = -h;
+      } else if (v < 0) {
+        hDiff = +!h;
+      }
+    } else if (r === 3) {
+      if (v !== 0) {
+        hDiff = -h;
+      }
+    } else if (r > 3) {
+      if (v > 0) {
+        hDiff = +!h;
+      } else if (v < 0) {
+        hDiff = -h;
+      }
     }
-};
 
-function createGetPositionsInDirections (r, c) {
-    return function getPositionsInDirections (...directions) {
-        let nextPosition = [r, c];
-        return directions.map(
-            direction => nextPosition = nextPosition && get(nextPosition).getPositionInDirection(direction)
-        );
+    const coords = [r - v, c + hDiff];
+
+    if (inBoard(coords)) {
+      return coords;
     }
+  };
 }
 
-function createGetPositionAfterDirections (r, c) {
-    return function getPositionAfterDirections (...directions) {
-        return this.getPositionsInDirections(...directions).slice(-1)[0];
-    }
+function createGetPositionsInDirections(r, c) {
+  return function getPositionsInDirections(...directions) {
+    let nextPosition = [r, c];
+    return directions.map(
+      direction =>
+        (nextPosition =
+          nextPosition && get(nextPosition).getPositionInDirection(direction)),
+    );
+  };
 }
 
-function createGetPositionsInDirection (r, c) {
-    return function getPositionsInDirection ([v, h] = [], positions = []) {
-        const currentPosition = positions[positions.length - 1] || [r, c];
-        const nextPosition = get(currentPosition).getPositionInDirection([v, h]);
-        if (!inBoard(nextPosition)) {
-            return positions;
-        }
-
-        return this.getPositionsInDirection([v, h], positions.concat([nextPosition]));
-    }
+function createGetPositionAfterDirections(r, c) {
+  return function getPositionAfterDirections(...directions) {
+    return this.getPositionsInDirections(...directions).slice(-1)[0];
+  };
 }
 
-function getVerticalDirection (from, to) {
-    if (from[0] > to[0]) {return 1;}
+function createGetPositionsInDirection(r, c) {
+  return function getPositionsInDirection([v, h] = [], positions = []) {
+    const currentPosition = positions[positions.length - 1] || [r, c];
+    const nextPosition = get(currentPosition).getPositionInDirection([v, h]);
+    if (!inBoard(nextPosition)) {
+      return positions;
+    }
 
-    if (from[0] === to[0]) {return 0}
-
-    if (from[0] < to[0]) {return -1}
+    return this.getPositionsInDirection(
+      [v, h],
+      positions.concat([nextPosition]),
+    );
+  };
 }
 
-function getHorizontalDirection (from, to, v) {
-    if (v === 0) {
-        return goingLeftDecreasesHorizontal(from, to);
-    }
+function getVerticalDirection(from, to) {
+  if (from[0] > to[0]) {
+    return 1;
+  }
 
-    if (from[0] < 3) {
-        return getUpperHalfDirection(from, to, v);
-    }
+  if (from[0] === to[0]) {
+    return 0;
+  }
 
-    if (from[0] === 3) {
-        return goingLeftDecreasesHorizontal(from, to);
-    }
-
-    if (from[0] > 3) {
-        return getLowerHalfDirection(from, to, v);
-    }
+  if (from[0] < to[0]) {
+    return -1;
+  }
 }
 
-function getUpperHalfDirection (from, to, v) {
-    if (v === 1) {
-        return goingLeftDecreasesHorizontal(from, to);
-    }
+function getHorizontalDirection(from, to, v) {
+  if (v === 0) {
+    return goingLeftDecreasesHorizontal(from, to);
+  }
 
-    if (v === -1) {
-        return goingRightIncreasesHorizontal(from, to);
-    }
+  if (from[0] < 3) {
+    return getUpperHalfDirection(from, to, v);
+  }
+
+  if (from[0] === 3) {
+    return goingLeftDecreasesHorizontal(from, to);
+  }
+
+  if (from[0] > 3) {
+    return getLowerHalfDirection(from, to, v);
+  }
 }
 
-function getLowerHalfDirection (from, to, v) {
-    if (v === 1) {
-        return goingRightIncreasesHorizontal(from, to);
-    }
+function getUpperHalfDirection(from, to, v) {
+  if (v === 1) {
+    return goingLeftDecreasesHorizontal(from, to);
+  }
 
-    if (v === -1) {
-        return goingLeftDecreasesHorizontal(from, to);
-    }
+  if (v === -1) {
+    return goingRightIncreasesHorizontal(from, to);
+  }
 }
 
-function goingLeftDecreasesHorizontal (from, to) {
-    return +(from[1] > to[1]);
+function getLowerHalfDirection(from, to, v) {
+  if (v === 1) {
+    return goingRightIncreasesHorizontal(from, to);
+  }
+
+  if (v === -1) {
+    return goingLeftDecreasesHorizontal(from, to);
+  }
 }
 
-function goingRightIncreasesHorizontal (from, to) {
-    return +(from[1] >= to[1]);
+function goingLeftDecreasesHorizontal(from, to) {
+  return +(from[1] > to[1]);
+}
+
+function goingRightIncreasesHorizontal(from, to) {
+  return +(from[1] >= to[1]);
 }
 
 // function getAdjacentCells (r, c) {
@@ -137,76 +148,79 @@ function goingRightIncreasesHorizontal (from, to) {
 
 const allCells = [];
 
-cellsByRow.forEach((numberOfCells) => {
-    const row = [];
-    for (let c = 0; c < numberOfCells; c++) {
-        const r = cells.length;
-        row.push({
-            // adjacentCells: getAdjacentCells(r, c),
-            position: [r, c],
-            getPositionInDirection: createGetPositionInDirection(r, c),
-            getPositionsInDirections: createGetPositionsInDirections(r, c),
-            getPositionAfterDirections: createGetPositionAfterDirections(r, c),
-            getPositionsInDirection: createGetPositionsInDirection(r, c)
-        });
+cellsByRow.forEach(numberOfCells => {
+  const row = [];
+  for (let c = 0; c < numberOfCells; c++) {
+    const r = cells.length;
+    row.push({
+      // adjacentCells: getAdjacentCells(r, c),
+      position: [r, c],
+      getPositionInDirection: createGetPositionInDirection(r, c),
+      getPositionsInDirections: createGetPositionsInDirections(r, c),
+      getPositionAfterDirections: createGetPositionAfterDirections(r, c),
+      getPositionsInDirection: createGetPositionsInDirection(r, c),
+    });
 
-        allCells.push([r, c]);
-    }
+    allCells.push([r, c]);
+  }
 
-    cells.push(row);
+  cells.push(row);
 });
 
-function get ([r, c] = outPosition) {
-    if (inBoard([r, c])) {
-        return cells[r][c];
+function get([r, c] = outPosition) {
+  if (inBoard([r, c])) {
+    return cells[r][c];
+  }
+
+  return {
+    position: outPosition,
+    getPositionInDirection: () => outPosition,
+    getPositionsInDirections: () => [outPosition],
+    getPositionAfterDirections: () => outPosition,
+    getPositionsInDirection: () => [outPosition],
+  };
+}
+
+function getAllAvailableCells() {
+  return allCells;
+}
+
+function getDirection(from, to) {
+  const v = getVerticalDirection(from, to);
+  const h = getHorizontalDirection(from, to, v);
+
+  return [v, h];
+}
+
+function inBoard([r, c] = outPosition) {
+  if (r >= 0 && r < 7) {
+    if (c >= 0 && c < cellsByRow[r]) {
+      return true;
     }
-
-    return {
-        position: outPosition,
-        getPositionInDirection: () => outPosition,
-        getPositionsInDirections: () => [outPosition],
-        getPositionAfterDirections: () => outPosition,
-        getPositionsInDirection: () => [outPosition]
-    };
+  }
 }
 
-function getAllAvailableCells () {
-    return allCells;
-}
+function getMovementCells(from, to) {
+  if (from && from.length) {
+    return (function concatCell(acc, cell) {
+      if (areCoordsEqual(cell, to)) {
+        return acc.concat([cell]);
+      }
 
-function getDirection (from, to) {
-    const v = getVerticalDirection(from, to);
-    const h = getHorizontalDirection(from, to, v);
+      return concatCell(
+        acc.concat([cell]),
+        get(cell).getPositionInDirection(getDirection(cell, to)),
+      );
+    })([from], get(from).getPositionInDirection(getDirection(from, to)));
+  }
 
-    return [v, h];
-}
-
-function inBoard ([r, c] = outPosition) {
-    if (r >= 0 && r < 7) {
-        if (c >= 0 && c < cellsByRow[r]) {
-            return true;
-        }
-    }
-}
-
-function getMovementCells (from, to) {
-    if (from && from.length) {
-        return (function concatCell (acc, cell) {
-            if (areCoordsEqual(cell, to)) {
-                return acc.concat([cell]);
-            }
-
-            return concatCell(acc.concat([cell]), get(cell).getPositionInDirection(getDirection(cell, to)));
-        })([from], get(from).getPositionInDirection(getDirection(from, to)));
-    }
-
-    return [to];
+  return [to];
 }
 
 export default {
-    get,
-    getAllAvailableCells,
-    getDirection,
-    inBoard,
-    getMovementCells
+  get,
+  getAllAvailableCells,
+  getDirection,
+  inBoard,
+  getMovementCells,
 };
